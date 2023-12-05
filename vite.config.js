a62c5dbde350c04e583807172ccee01e706d5ebd
TinWinHlaing-777 <tiniwinhlaingmm777@gmail.com>
2023-12-05 11:42:00 +0630

    updated version conflict

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "react";
+import reactDom from "react-dom";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [react, reactDom],
+    },
+  },
+});
